69c6bb53dabfb7ccc06918711a7f5957a8c34ed5
sgudkeys <Simon.DeKeyser@UGent.be>
2020-11-29 18:52:35 +0100

    added some plot functions, see them in main_thin.py

--- main_thin.py
+++ main_thin.py
@@ -1,50 +1,49 @@
 import numpy as np
 from SimulationEngine import FDTD
 import matplotlib.pyplot as plt
+from scipy.special import hankel2, hankel1
 
 # PARAMETERS----------------------------------------------
 c = 340  # geluidssnelheid - speed of sound (wave speed)
-dx=0.05
-kd=5
-CFL = 0.8  # Courant number
+dx= 0.2
+kd = 3
+d=2
+nd = d/dx
+CFL = 1  # Courant number
 dt = CFL / (c * np.sqrt((1 / dx ** 2) + (1 / dx ** 2)))  # time step
-nt = 1000
+nt = 400
 obj = 'thin' # Object to simulate
 
 # SIMULATION----------------------------------------------
-thin = FDTD(dx,kd,dt,nt,obj,animation=False)
-thin.run()
-freefield = FDTD(dx,kd,dt,nt,'freefield_'+obj,animation=False)
-#freefield.run()
-recorders = np.load('freefield_thin_recorders_kd=5.npy') #freefield was saved (large simulation)
-source = np.load('freefield_thin_source_kd=5.npy')
+#thin = FDTD(dx,kd,dt,nt,obj,animation=False)
+#thin.run()
+freefield = FDTD(dx,kd,dt,nt,'freefield_'+obj,animation=True)
+freefield.run()
+recorders = np.load('freefield_thin_recorders_kd={}.npy'.format(kd)) #freefield was saved (large simulation)
+source = np.load('freefield_thin_source_kd={}.npy'.format(kd))
 
 # PLOT----------------------------------------------------
-thin.plot_recorders()
-thin.plot_source()
-freefield.plot_recorders(recorders=recorders)
+time = np.arange(nt)*dt
+FFT_source, k_vec = freefield.fft(source,8000)
+FFT_1, _ = freefield.fft(recorders[0],8000)
+FFT_2, _ = freefield.fft(recorders[1],8000)
+freefield.plot_time_fft(time,source,recorders[0],k_vec,FFT_source,FFT_1) #plot a summary
 
 # ANALYTICAL COMPARISON------------------------------------
-TF_ana, k_vec = thin.TF_ANA(recorder_number=1)
-TF_freefield, k_fft = freefield.TF_SIM(recorder_number=1,recorders=recorders,source=source)
-TF_thin, k_fft = thin.TF_SIM(recorder_number=1)
+TF_1, k_vec = freefield.TF_SIM(recorder_number=1,recorders=recorders,source=source, plot= True)
+r1 = np.sqrt((2)**2+(2/5)**2)*d #distance to first recorder
+TF_ana = hankel1(0,k_vec*r1)/(FFT_source)
+freefield.plot_FDTD_ana(k_vec,TF_ana,TF_1) #plot a FDTD/analytical comparison
 
-TF = TF_thin-TF_freefield
-Amp = np.abs(TF)
-Phase = np.unwrap(np.angle(TF)) 
-Amp_ana = np.abs(TF_ana)
-Phase_ana = np.unwrap(np.angle(TF_ana)) 
-ax1 = plt.subplot(2,1,1)
-ax1.plot(k_vec,Amp_ana,label='Analytical')
-ax1.plot(k_fft,Amp,label='Simulated')
-ax1.set_ylabel('Amplitude')
-ax1.set_title('Transfer function (freefield subtracted), thin sheet case')
-ax1.grid()
-ax2 = plt.subplot(2,1,2)
-ax2.plot(k_vec,Phase_ana,label='Analytical')
-ax2.plot(k_fft,Phase,label='Simulated')
-ax2.set_xlabel('kd')
-ax2.set_ylabel('Phase')
-ax2.grid()
-plt.legend()
+## WANHOPIGE CONTROLE MET PC LES 1
+r2 = np.sqrt((3)**2+(2/5)**2)*d #distance to second recorder
+Averhouding_FDTD = np.abs(FFT_1/FFT_2)
+Averhouding_theo = np.abs(hankel1(0,k_vec*r1)/hankel1(0,k_vec*r2))
+Averhoudingrel = Averhouding_FDTD/Averhouding_theo
+Averhouding = 1+((Averhoudingrel-1)/nd)
+
+plt.plot(2*np.pi/(k_vec*dx),Averhouding)
+plt.title('ratio fft recorder1/recorder2 vs analytical')
+plt.ylabel('ratio')
+plt.xlabel('number of cells per wavelength')
 plt.show()
